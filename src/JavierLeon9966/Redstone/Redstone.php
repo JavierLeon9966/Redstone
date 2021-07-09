f009fedaa7e426e0a2709e58e0d4f2c7d5e81c7c
JavierLeon9966 <javierleonalarcon14@gmail.com>
2021-07-08 22:24:45 -0300

    Upload Files

---
+++ src/JavierLeon9966/Redstone/Redstone.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace JavierLeon9966\Redstone;
+
+use pocketmine\plugin\PluginBase;
+
+final class Redstone extends PluginBase{
+	public function onLoad(): void{
+		
+	}
+}
